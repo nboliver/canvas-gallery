9ffa4fc299435c1be6e8ebdcfad3175d3f31b1f4
nboliver <nic.b.oliver@gmail.com>
2016-10-30 11:18:10 -0700

    First commit

---
+++ includes/class-canvas-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://nboliver.com
+ * @since      1.0.0
+ *
+ * @package    Canvas
+ * @subpackage Canvas/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Canvas
+ * @subpackage Canvas/includes
+ * @author     Nic Oliver <nic@nboliver.com>
+ */
+class Canvas_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-canvas-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://nboliver.com
+ * @since      1.0.0
+ *
+ * @package    Canvas
+ * @subpackage Canvas/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Canvas
+ * @subpackage Canvas/includes
+ * @author     Nic Oliver <nic@nboliver.com>
+ */
+class Canvas_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
